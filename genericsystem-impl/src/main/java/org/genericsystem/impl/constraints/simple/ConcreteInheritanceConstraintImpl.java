528ef7719905aa6c75a8ec47d0f41261a96def8a
nfeybesse <nfeybesse@middlewarefactory.com>
2013-01-15 18:10:22 +0100

    add concrete

---
+++ genericsystem-impl/src/main/java/org/genericsystem/impl/constraints/simple/ConcreteInheritanceConstraintImpl.java
@@ -0,0 +1,47 @@
+package org.genericsystem.impl.constraints.simple;
+
+import java.io.Serializable;
+import org.genericsystem.api.annotation.BooleanValue;
+import org.genericsystem.api.annotation.Components;
+import org.genericsystem.api.annotation.Dependencies;
+import org.genericsystem.api.annotation.Interfaces;
+import org.genericsystem.api.annotation.SystemGeneric;
+import org.genericsystem.api.annotation.constraints.PropertyConstraint;
+import org.genericsystem.api.annotation.constraints.SingularConstraint;
+import org.genericsystem.api.core.Context;
+import org.genericsystem.api.core.Engine;
+import org.genericsystem.api.core.Generic;
+import org.genericsystem.api.exception.ConcreteInheritanceConstraintViolationException;
+import org.genericsystem.api.exception.ConstraintViolationException;
+import org.genericsystem.impl.core.GenericImpl;
+import org.genericsystem.impl.core.Statics;
+
+@SystemGeneric
+@Components(Engine.class)
+@Dependencies(ConcreteInheritanceConstraintImpl.DefaultValue.class)
+@PropertyConstraint
+@SingularConstraint(Statics.BASE_POSITION)
+public class ConcreteInheritanceConstraintImpl extends AbstractSimpleBooleanConstraint {
+	
+	private static final long serialVersionUID = -6429972259714036057L;
+	
+	@Override
+	protected void internalCheck(Context context, Generic modified, Generic constraintBaseType) throws ConstraintViolationException {
+		if (modified.isConcrete() && ((GenericImpl) modified).isPrimary() && !((GenericImpl) modified).isPhantom())
+			if (((GenericImpl) modified).getSupers().first().isConcrete())
+				throw new ConcreteInheritanceConstraintViolationException("" + modified);
+	}
+	
+	@SystemGeneric(SystemGeneric.CONCRETE)
+	@Components(Engine.class)
+	@BooleanValue(true)
+	@Interfaces(ConcreteInheritanceConstraintImpl.class)
+	public static class DefaultValue {}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public <T extends Serializable> T getDefaultValue(Generic generic) {
+		return (T) Boolean.TRUE;
+	}
+	
+}
---
+++ genericsystem-impl/src/main/java/org/genericsystem/impl/iterator/AbstractSelectableLeafInheritedIterator.java
@@ -0,0 +1,30 @@
+package org.genericsystem.impl.iterator;
+
+import java.util.Iterator;
+
+import org.genericsystem.api.core.Context;
+import org.genericsystem.api.core.Generic;
+import org.genericsystem.impl.core.GenericImpl;
+
+public abstract class AbstractSelectableLeafInheritedIterator extends AbstractSelectablePostTreeIterator<Generic> {
+
+	private final Context context;
+
+	public AbstractSelectableLeafInheritedIterator(Context context, Generic root) {
+		super(root);
+		this.context = context;
+	}
+
+	@Override
+	protected Iterator<Generic> children(final Generic father) {
+		return new AbstractFilterIterator<Generic>(((GenericImpl) father).directInheritingsIterator(context)) {
+			@Override
+			public boolean isSelected() {
+				return AbstractSelectableLeafInheritedIterator.this.isSelected(next);
+			}
+		};
+	}
+
+	protected abstract boolean isSelected(Generic candidate);
+
+}
