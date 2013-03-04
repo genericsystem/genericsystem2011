a28b68b815811495daabff563007e9b9a7267413
mory <mory@middlewarefactory.com>
2013-03-04 15:51:31 +0100

    fix rename package name

--- genericsystem-api/src/main/java/org/genericsystem/core/Factory.java
+++ genericsystem-api/src/main/java/org/genericsystem/core/Factory.java
@@ -54,31 +54,16 @@ public interface Factory extends Serializable {
 		@SuppressWarnings({ "static-access", "unchecked" })
 		public DefaultFactory(Class<?>... classes) {
 			try {
-				engineConstructor = this
-						.<Engine> getImplementation(
-								(Class<Engine>) Class
-										.forName("org.genericsystem.impl.core.EngineImpl"),
-								classes).getConstructor(Config.class,
-								Class[].class);
-				genericClass = this
-						.<Generic> getImplementation(
-								(Class<Generic>) Class
-										.forName("org.genericsystem.impl.core.GenericImpl"),
-								classes);
-				cacheConstructor = this
-						.<Cache> getImplementation(
-								(Class<Cache>) Class
-										.forName("org.genericsystem.impl.core.CacheImpl"),
-								classes).getConstructor(Context.class);
-			} catch (ClassNotFoundException | NoSuchMethodException
-					| SecurityException e) {
+				engineConstructor = this.<Engine> getImplementation((Class<Engine>) Class.forName("org.genericsystem.core.EngineImpl"), classes).getConstructor(Config.class, Class[].class);
+				genericClass = this.<Generic> getImplementation((Class<Generic>) Class.forName("org.genericsystem.core.GenericImpl"), classes);
+				cacheConstructor = this.<Cache> getImplementation((Class<Cache>) Class.forName("org.genericsystem.core.CacheImpl"), classes).getConstructor(Context.class);
+			} catch (ClassNotFoundException | NoSuchMethodException | SecurityException e) {
 				throw new IllegalStateException(e);
 			}
 		}
 
 		@SuppressWarnings("unchecked")
-		private static <T> Class<T> getImplementation(Class<T> interfaceClass,
-				Class<?>[] classes) {
+		private static <T> Class<T> getImplementation(Class<T> interfaceClass, Class<?>[] classes) {
 			for (Class<?> clazz : classes)
 				if (interfaceClass.isAssignableFrom(clazz)) {
 					if (!Generic.class.equals(interfaceClass))
@@ -93,8 +78,7 @@ public interface Factory extends Serializable {
 		public Engine newEngine(Config config, Class<?>... userClasses) {
 			try {
 				return engineConstructor.newInstance(config, userClasses);
-			} catch (InstantiationException | IllegalAccessException
-					| IllegalArgumentException | InvocationTargetException e) {
+			} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
 				throw new IllegalStateException(e);
 			}
 		}
@@ -103,8 +87,7 @@ public interface Factory extends Serializable {
 		public Generic newGeneric() {
 			try {
 				return genericClass.newInstance();
-			} catch (InstantiationException | IllegalAccessException
-					| IllegalArgumentException e) {
+			} catch (InstantiationException | IllegalAccessException | IllegalArgumentException e) {
 				throw new IllegalStateException(e);
 			}
 		}
@@ -113,8 +96,7 @@ public interface Factory extends Serializable {
 		public Cache newCache(Context context) {
 			try {
 				return cacheConstructor.newInstance(context);
-			} catch (InstantiationException | IllegalAccessException
-					| IllegalArgumentException | InvocationTargetException e) {
+			} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
 				throw new IllegalStateException(e);
 			}
 		}
