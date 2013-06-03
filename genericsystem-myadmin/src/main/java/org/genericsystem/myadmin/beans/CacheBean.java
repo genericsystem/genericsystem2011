24e9b3fb50c48cf05bec8572b6fd42cf2ca22016
scollet <scollet@middlewarefactory.com>
2013-06-03 14:11:32 +0200

    Manage of super cache.

--- genericsystem-myadmin/src/main/java/org/genericsystem/myadmin/beans/CacheBean.java
+++ genericsystem-myadmin/src/main/java/org/genericsystem/myadmin/beans/CacheBean.java
@@ -1,16 +1,22 @@
 package org.genericsystem.myadmin.beans;
 
 import java.io.Serializable;
+
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+
 import org.genericsystem.core.Cache;
+import org.genericsystem.myadmin.util.GsMessages;
 
 @Named
 @SessionScoped
 public class CacheBean implements Serializable {
 
 	private static final long serialVersionUID = 1752488068961882618L;
+
+	@Inject
+	private GsMessages messages;
 
 	@Inject
 	private transient Cache cache;
@@ -24,4 +30,9 @@ public class CacheBean implements Serializable {
 		return "HOME";
 	}
 
+	public String newCache() {
+		Cache superCache = cache.newSuperCache();
+		messages.info("addCache");
+		return "HOME";
+	}
 }
