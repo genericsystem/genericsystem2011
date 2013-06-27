0e3b1661a56cc610bf82e438ecdecd06e83bbe5c
scollet <scollet@middlewarefactory.com>
2013-06-27 18:44:34 +0200

    Fix bug on Constraint class when adding attributes of simple type with drag & drop. Drag & drop for relations.

--- genericsystem-myadmin/src/main/java/org/genericsystem/myadmin/beans/TreeBean.java
+++ genericsystem-myadmin/src/main/java/org/genericsystem/myadmin/beans/TreeBean.java
@@ -1,9 +1,10 @@
 package org.genericsystem.myadmin.beans;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
 import javax.enterprise.event.Event;
 import javax.enterprise.event.Observes;
 import javax.faces.event.PhaseEvent;
@@ -14,10 +15,18 @@ import org.jboss.seam.faces.event.qualifier.After;
 import org.jboss.seam.faces.event.qualifier.InvokeApplication;
 import org.richfaces.component.UITree;
 import org.richfaces.event.TreeSelectionChangeEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Named
-@RequestScoped
-public class TreeBean {
+// @RequestScoped
+@SessionScoped
+public class TreeBean implements Serializable {
+
+	private static final long serialVersionUID = 5254125940306737835L;
+
+	// TODO clean
+	private static Logger log = LoggerFactory.getLogger(TypesBean.class);
 
 	@Inject
 	// @TreeSelection
@@ -25,15 +34,19 @@ public class TreeBean {
 
 	private TreeSelectionEvent event;
 
+	private boolean selectionLocked;
+
 	public void change(TreeSelectionChangeEvent selectionChangeEvent) {
-		List<Object> selection = new ArrayList<Object>(selectionChangeEvent.getNewSelection());
-		if (!selection.isEmpty()) {
-			Object currentSelectionKey = selection.get(0);
-			UITree tree = (UITree) selectionChangeEvent.getSource();
-			Object storedKey = tree.getRowKey();
-			tree.setRowKey(currentSelectionKey);
-			event = new TreeSelectionEvent(tree.getId(), tree.getRowData());
-			tree.setRowKey(storedKey);
+		if (!selectionLocked) {
+			List<Object> selection = new ArrayList<Object>(selectionChangeEvent.getNewSelection());
+			if (!selection.isEmpty()) {
+				Object currentSelectionKey = selection.get(0);
+				UITree tree = (UITree) selectionChangeEvent.getSource();
+				Object storedKey = tree.getRowKey();
+				tree.setRowKey(currentSelectionKey);
+				event = new TreeSelectionEvent(tree.getId(), tree.getRowData());
+				tree.setRowKey(storedKey);
+			}
 		}
 	}
 
@@ -42,6 +55,14 @@ public class TreeBean {
 			launcher.fire(event);
 			event = null;
 		}
+	}
+
+	public boolean isSelectionLocked() {
+		return selectionLocked;
+	}
+
+	public void setSelectionLocked(boolean selectionLocked) {
+		this.selectionLocked = selectionLocked;
 	}
 
 	public static class TreeSelectionEvent {
