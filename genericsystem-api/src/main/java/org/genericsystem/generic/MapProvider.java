37b581db79e8dc0f13f3cc75d300de9049bd740d
Alexei KLENIN <alexei.klenin@gmail.com>
2013-10-02 18:00:30 +0200

    Now MapProvider intreface extends Attribute interface.

--- genericsystem-api/src/main/java/org/genericsystem/generic/MapProvider.java
+++ genericsystem-api/src/main/java/org/genericsystem/generic/MapProvider.java
@@ -1,13 +1,14 @@
 package org.genericsystem.generic;
 
 import java.io.Serializable;
+
 import org.genericsystem.core.Generic;
 
 /**
  * @author Nicolas Feybesse
  * 
  */
-public interface MapProvider extends Type {
+public interface MapProvider extends Attribute {
 
 	<Key extends Serializable, Value extends Serializable> ExtendedMap<Key, Value> getMap(Generic generic);
 
