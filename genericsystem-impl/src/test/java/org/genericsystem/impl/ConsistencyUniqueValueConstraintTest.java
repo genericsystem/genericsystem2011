5004c2a30436d38403080145b3b134211ecaa46e
Ameni Ben Mbarek <abenmbarek@middlewarefactory.com>
2013-10-03 09:48:47 +0200

    commmit concistencyUniqueValue

--- genericsystem-impl/src/test/java/org/genericsystem/impl/ConsistencyUniqueValueConstraintTest.java
+++ genericsystem-impl/src/test/java/org/genericsystem/impl/ConsistencyUniqueValueConstraintTest.java
@@ -5,10 +5,12 @@ import org.genericsystem.core.Generic;
 import org.genericsystem.core.GenericSystem;
 import org.genericsystem.exception.UniqueValueConstraintViolationException;
 import org.genericsystem.generic.Attribute;
+import org.genericsystem.generic.Relation;
 import org.genericsystem.generic.Type;
 import org.testng.annotations.Test;
+
 @Test
-public class ConsistencyUniqueValueConstraintTest extends AbstractTest{
+public class ConsistencyUniqueValueConstraintTest extends AbstractTest {
 	public void PropertySimpleAttributeOK() {
 		Cache cache = GenericSystem.newCacheOnANewInMemoryEngine().start();
 		Type car = cache.newType("Car");
@@ -29,9 +31,47 @@ public class ConsistencyUniqueValueConstraintTest extends AbstractTest{
 		new RollbackCatcher() {
 			@Override
 			public void intercept() {
-				
+
 				registration.enableUniqueValueConstraint();
 			}
 		}.assertIsCausedBy(UniqueValueConstraintViolationException.class);
+	}
+
+	public void PropertySimpleRelationOK() {
+		final Cache cache = GenericSystem.newCacheOnANewInMemoryEngine()
+				.start();
+		Type car = cache.newType("Car");
+		Type human = cache.newType("Human");
+		final Relation driving = car.setRelation("DrivingAlong", human);
+		Generic myCar = car.newInstance("myCar");
+		Generic yourCar = car.newInstance("yourCar");
+		Generic myHuman = human.newInstance("myHuman");
+		Generic yourHuman = human.newInstance("yourHuman");
+		myCar.setLink(driving, "my_driving", myHuman);
+		yourCar.setLink(driving, "your_driving", yourHuman);
+		driving.enableUniqueValueConstraint();
+	}
+
+	public void PropertySimpleRelationKO() {
+		final Cache cache = GenericSystem.newCacheOnANewInMemoryEngine()
+				.start();
+		Type car = cache.newType("Car");
+		Type human = cache.newType("Human");
+		final Relation driving = car.setRelation("DrivingAlong", human);
+		Generic myCar = car.newInstance("myCar");
+		Generic yourCar = car.newInstance("yourCar");
+		Generic myHuman = human.newInstance("myHuman");
+		Generic yourHuman = human.newInstance("yourHuman");
+		myCar.setLink(driving, "my_driving", myHuman);
+		yourCar.setLink(driving, "your_driving", yourHuman);
+		yourCar.setLink(driving, "my_driving", yourHuman);
+		new RollbackCatcher() {
+			@Override
+			public void intercept() {
+
+				driving.enableUniqueValueConstraint();
+			}
+		}.assertIsCausedBy(UniqueValueConstraintViolationException.class);
+
 	}
 }
