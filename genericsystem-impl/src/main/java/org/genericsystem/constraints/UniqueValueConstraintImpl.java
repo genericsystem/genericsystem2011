54d13f01aeb236fa6f086796308aecb40a68f045
Alexei KLENIN <alexei.klenin@gmail.com>
2013-10-21 15:02:21 +0200

    Constraint UniqueValueConstraint is not more default.

--- genericsystem-impl/src/main/java/org/genericsystem/constraints/UniqueValueConstraintImpl.java
+++ genericsystem-impl/src/main/java/org/genericsystem/constraints/UniqueValueConstraintImpl.java
@@ -3,19 +3,16 @@ package org.genericsystem.constraints;
 import java.util.Arrays;
 
 import org.genericsystem.annotation.Components;
-import org.genericsystem.annotation.Dependencies;
 import org.genericsystem.annotation.Extends;
 import org.genericsystem.annotation.SystemGeneric;
 import org.genericsystem.annotation.constraints.SingularConstraint;
 import org.genericsystem.annotation.value.AxedConstraintValue;
-import org.genericsystem.annotation.value.BooleanValue;
 import org.genericsystem.core.Generic;
 import org.genericsystem.core.GenericImpl;
 import org.genericsystem.exception.ConstraintViolationException;
 import org.genericsystem.exception.UniqueValueConstraintViolationException;
 import org.genericsystem.generic.Holder;
 import org.genericsystem.generic.Type;
-import org.genericsystem.map.ConstraintsMapProvider;
 import org.genericsystem.map.ConstraintsMapProvider.ConstraintKey;
 import org.genericsystem.map.ConstraintsMapProvider.MapInstance;
 
@@ -28,16 +25,16 @@ import org.genericsystem.map.ConstraintsMapProvider.MapInstance;
 @Extends(meta = ConstraintKey.class)
 @Components(MapInstance.class)
 @SingularConstraint
-@Dependencies(UniqueValueConstraintImpl.DefaultValue.class)
+//@Dependencies(UniqueValueConstraintImpl.DefaultValue.class)
 @AxedConstraintValue(UniqueValueConstraintImpl.class)
 public class UniqueValueConstraintImpl extends AbstractBooleanConstraintImpl implements Holder {
 
-	@SystemGeneric
-	@Extends(meta = ConstraintsMapProvider.ConstraintValue.class)
-	@Components(UniqueValueConstraintImpl.class)
-	@BooleanValue(true)
-	public static class DefaultValue extends GenericImpl implements Holder {
-	}
+	//@SystemGeneric
+	//@Extends(meta = ConstraintsMapProvider.ConstraintValue.class)
+	//@Components(UniqueValueConstraintImpl.class)
+	//@BooleanValue(true)
+	//public static class DefaultValue extends GenericImpl implements Holder {
+	//}
 
 	/**
 	 * {@inheritDoc}
