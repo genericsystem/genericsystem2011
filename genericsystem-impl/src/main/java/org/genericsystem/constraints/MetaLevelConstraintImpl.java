4cc4c3a6d8dab8ddff223a440bc36c2c5f678905
Francois des ROCHETTES <fdesrochettes@middlewarefactory.com>
2013-12-20 17:41:16 +0100

    Add constraint on metaLevel.

---
+++ genericsystem-impl/src/main/java/org/genericsystem/constraints/MetaLevelConstraintImpl.java
@@ -0,0 +1,43 @@
+package org.genericsystem.constraints;
+
+import org.genericsystem.annotation.Components;
+import org.genericsystem.annotation.Dependencies;
+import org.genericsystem.annotation.Extends;
+import org.genericsystem.annotation.SystemGeneric;
+import org.genericsystem.annotation.value.AxedConstraintValue;
+import org.genericsystem.annotation.value.BooleanValue;
+import org.genericsystem.constraints.AbstractConstraintImpl.AbstractBooleanNoAxedConstraintImpl;
+import org.genericsystem.core.Generic;
+import org.genericsystem.exception.ConstraintViolationException;
+import org.genericsystem.exception.MetaLevelConstraintViolationException;
+import org.genericsystem.map.ConstraintsMapProvider;
+import org.genericsystem.map.ConstraintsMapProvider.ConstraintKey;
+
+@SystemGeneric
+@Extends(ConstraintKey.class)
+@Components(ConstraintsMapProvider.class)
+@Dependencies({ MetaLevelConstraintImpl.DefaultKey.class, MetaLevelConstraintImpl.DefaultValue.class })
+public class MetaLevelConstraintImpl extends AbstractBooleanNoAxedConstraintImpl {
+
+	@SystemGeneric
+	@Extends(meta = MetaLevelConstraintImpl.class)
+	@Components(ConstraintsMapProvider.class)
+	@AxedConstraintValue(MetaLevelConstraintImpl.class)
+	public static class DefaultKey {
+	}
+
+	@SystemGeneric
+	@Extends(meta = ConstraintsMapProvider.ConstraintValue.class)
+	@Components(DefaultKey.class)
+	@BooleanValue(true)
+	public static class DefaultValue {
+	}
+
+	@Override
+	public void check(Generic constraintBase, Generic modified) throws ConstraintViolationException {
+		for (Generic component : modified.getComponents())
+			if (component.getMetaLevel() > modified.getMetaLevel())
+				throw new MetaLevelConstraintViolationException(component + " must have a meta level lower than " + modified);
+	}
+
+}
--- genericsystem-myadmin/src/main/java/org/genericsystem/myadmin/beans/CacheBean.java
+++ genericsystem-myadmin/src/main/java/org/genericsystem/myadmin/beans/CacheBean.java
@@ -1,6 +1,8 @@
 package org.genericsystem.myadmin.beans;
 
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.enterprise.event.Observes;
 import javax.faces.bean.RequestScoped;
@@ -11,6 +13,7 @@ import javax.inject.Named;
 import org.genericsystem.cdi.CacheProvider;
 import org.genericsystem.core.Cache;
 import org.genericsystem.core.CacheImpl;
+import org.genericsystem.core.Statics;
 import org.genericsystem.exception.ConstraintViolationException;
 import org.genericsystem.myadmin.util.GsMessages;
 import org.jboss.seam.faces.event.qualifier.After;
@@ -77,7 +80,7 @@ public class CacheBean implements Serializable {
 	 * @return the formated timestamp of current cache.
 	 */
 	public String getCurrentCacheTs() {
-		return "";// new SimpleDateFormat(Statics.LOG_PATTERN).format(new Date(((CacheImpl) cacheProvider.getCurrentCache()).getTs() / Statics.MILLI_TO_NANOSECONDS)).toString();
+		return new SimpleDateFormat(Statics.LOG_PATTERN).format(new Date(((CacheImpl) cacheProvider.getCurrentCache()).getTs() / Statics.MILLI_TO_NANOSECONDS)).toString();
 	}
 
 	public Cache getCache() {
