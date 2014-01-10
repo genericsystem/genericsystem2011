c88cfdaf9611e2a6f8e12e67cafa5bfb74b4bec9
Hendrik <hendrik@middlewarefactory.com>
2014-01-10 13:45:03 +0100

    - Add type functionality implemented

---
+++ genericsystem-jsf-example/src/main/java/org/genericsystem/jsf/example/CarColorRelation.java
@@ -0,0 +1,10 @@
+package org.genericsystem.jsf.example;
+
+import org.genericsystem.annotation.Components;
+import org.genericsystem.annotation.SystemGeneric;
+
+@SystemGeneric
+@Components({ CarType.class, ColorType.class })
+public class CarColorRelation {
+
+}
