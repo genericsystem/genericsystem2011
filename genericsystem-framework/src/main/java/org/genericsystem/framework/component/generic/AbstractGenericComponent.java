7181dec5e69a8a072eaddc827b89bae1cdba9c77
Charles Vuillecard <cvuillecard@middlewarefactory.com>
2014-03-13 15:27:24 +0100

    minor modification...

--- genericsystem-framework/src/main/java/org/genericsystem/framework/component/generic/AbstractGenericComponent.java
+++ genericsystem-framework/src/main/java/org/genericsystem/framework/component/generic/AbstractGenericComponent.java
@@ -42,22 +42,3 @@ public abstract class AbstractGenericComponent extends AbstractComponent {
 	}
 
 }
-
-// public abstract class AbstractGenericComponent extends AbstractComponent {
-// private final Generic generic;
-//
-// public AbstractGenericComponent(AbstractComponent parent, Generic generic) {
-// super(parent);
-// this.generic = generic;
-// this.children = initChildren();
-// }
-//
-// public Generic getGeneric() {
-// return generic;
-// }
-//
-// @Override
-// public String toString() {
-// return Objects.toString(generic);
-// }
-// }
