47ba7b4b1bbbb57114c8250e8277b50fc51e6a7f
Charles Vuillecard <cvuillecard@middlewarefactory.com>
2014-03-24 17:27:13 +0100

    Re-builded base version of genericsystem-framework

--- genericsystem-framework/src/main/java/org/genericsystem/framework/component/AbstractComponent.java
+++ genericsystem-framework/src/main/java/org/genericsystem/framework/component/AbstractComponent.java
@@ -1,14 +1,8 @@
 package org.genericsystem.framework.component;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.genericsystem.core.Cache;
-import org.genericsystem.core.Generic;
-import org.genericsystem.core.Snapshot;
-import org.genericsystem.core.Snapshot.Filter;
-import org.genericsystem.core.Snapshot.Projector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,8 +21,6 @@ public abstract class AbstractComponent {
 	}
 
 	public abstract List<? extends AbstractComponent> initChildren();
-
-	public abstract <T extends Generic> Snapshot<T> getGenerics();
 
 	public abstract String getXhtmlPath();
 
@@ -50,39 +42,5 @@ public abstract class AbstractComponent {
 
 	public Cache getCache() {
 		return getRoot().getCache();
-	}
-
-	public abstract <T extends AbstractComponent, U extends Generic> T buildComponent(U generic);
-
-	public class ProjectorGeneric<T extends AbstractComponent, U extends Generic> implements Projector<T, U> {
-		private final Map<U, T> map = new HashMap<U, T>() {
-
-			private static final long serialVersionUID = -7927996818181180784L;
-
-			@SuppressWarnings("unchecked")
-			@Override
-			public T get(Object key) {
-				T result = super.get(key);
-				if (result == null)
-					put((U) key, result = buildComponent((U) key));
-				return result;
-			}
-		};
-
-		@Override
-		public T project(U element) {
-			return map.get(element);
-		}
-	};
-
-	public abstract <T extends Generic> boolean isSelected(T candidate);
-
-	public class FilterGeneric<T extends Generic> implements Filter<T> {
-
-		@Override
-		public boolean isSelected(T candidate) {
-			return AbstractComponent.this.isSelected(candidate);
-		}
-
 	}
 }
--- genericsystem-framework/src/main/java/org/genericsystem/framework/component/AbstractSelectorComponent.java
+++ genericsystem-framework/src/main/java/org/genericsystem/framework/component/AbstractSelectorComponent.java
@@ -1,26 +1,19 @@
 package org.genericsystem.framework.component;
 
-import java.util.List;
-
 import org.genericsystem.core.Generic;
 
-public abstract class AbstractSelectorComponent extends AbstractComponent {
-
-	private AbstractComponent child;
+public abstract class AbstractSelectorComponent extends AbstractCollectableChildrenComponent {
 
 	public AbstractSelectorComponent(AbstractComponent rootComponent) {
 		super(rootComponent);
 		selectDefaultComponent();
 	}
 
-	@Override
-	public List<? extends AbstractComponent> initChildren() {
-		return getGenerics().filter(new FilterGeneric<Generic>()).project(new ProjectorGeneric<AbstractComponent, Generic>());
-	}
-
 	public abstract void selectDefaultComponent();
 
 	public abstract void select(Generic selected);
+
+	private AbstractComponent child;
 
 	@SuppressWarnings("unchecked")
 	public <T extends AbstractComponent> T getChild() {
