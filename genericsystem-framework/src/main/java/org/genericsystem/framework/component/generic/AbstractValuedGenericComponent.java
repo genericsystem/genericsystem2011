f33cf08b2f7c91dc75bbc62d243075a741e8f2dd
Charles Vuillecard <cvuillecard@middlewarefactory.com>
2014-03-25 11:16:14 +0100

    nothing

--- genericsystem-framework/src/main/java/org/genericsystem/framework/component/generic/AbstractValuedGenericComponent.java
+++ genericsystem-framework/src/main/java/org/genericsystem/framework/component/generic/AbstractValuedGenericComponent.java
@@ -5,7 +5,7 @@ import org.genericsystem.framework.component.AbstractComponent;
 import org.genericsystem.framework.component.ValuedComponent;
 
 public abstract class AbstractValuedGenericComponent extends AbstractGenericComponent implements ValuedComponent {
-	private String newValue;
+	protected String newValue;
 
 	public AbstractValuedGenericComponent(AbstractComponent parent, Generic generic) {
 		super(parent, generic);
