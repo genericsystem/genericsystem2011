57cdc7ac44098a2b696a87d709cd68a81d700dc6
Charles Vuillecard <cvuillecard@middlewarefactory.com>
2014-03-27 10:28:14 +0100

    rectification in getNewValue() -> AbstractEditAndCreateComponent

--- genericsystem-tracker/src/main/java/org/genericsystem/tracker/component/generic/CreateAndEditComponent.java
+++ genericsystem-tracker/src/main/java/org/genericsystem/tracker/component/generic/CreateAndEditComponent.java
@@ -6,7 +6,7 @@ import java.util.Objects;
 import org.genericsystem.core.Generic;
 import org.genericsystem.core.Snapshot;
 import org.genericsystem.framework.component.AbstractComponent;
-import org.genericsystem.framework.component.generic.AbstractGenericCollectableChildrenComponent;
+import org.genericsystem.framework.component.generic.AbstractCreateAndEditComponent;
 import org.genericsystem.framework.component.generic.GenericComponent;
 import org.genericsystem.generic.Attribute;
 import org.genericsystem.generic.Relation;
@@ -15,20 +15,23 @@ import org.genericsystem.tracker.InstanceRow;
 import org.genericsystem.tracker.structure.Attributes;
 import org.genericsystem.tracker.structure.Relations;
 
-public class EditComponent extends AbstractGenericCollectableChildrenComponent {
+@SuppressWarnings("unchecked")
+public class CreateAndEditComponent extends AbstractCreateAndEditComponent {
+	private String title;
 
-	private String newValue;
-
-	public EditComponent(AbstractComponent parent, Generic generic) {
+	public CreateAndEditComponent(AbstractComponent parent, Generic generic) {
 		super(parent, generic);
-	}
-
-	public EditComponent(TypeComponent typeComponent, InstanceRow instanceRow) {
-		this(typeComponent, instanceRow.getInstance());
 		initChildren();
+		setTitle("add");
 	}
 
-	@SuppressWarnings("unchecked")
+	public CreateAndEditComponent(TypeComponent typeComponent, InstanceRow instanceRow) {
+		this(typeComponent, instanceRow.getInstance());
+		setMode(MODE.EDITION);
+		setTitle("update");
+		setNewValue(Objects.toString(getGeneric().toString()));
+	}
+
 	@Override
 	public <T extends Generic> Snapshot<T> getGenerics() {
 		return (Snapshot<T>) ((Type) getGeneric()).getAttributes();
@@ -40,20 +43,33 @@ public class EditComponent extends AbstractGenericCollectableChildrenComponent {
 		return clazz != null && (Attributes.class.equals(clazz) || Relations.class.equals(clazz));
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override
 	public <T extends AbstractComponent, U extends Generic> T buildComponent(U generic) {
-		return (T) new RowComponent(EditComponent.this, generic);
+		return (T) new RowComponent(CreateAndEditComponent.this, generic);
 	}
 
-	public String getInstanceName() {
-		return Objects.toString(getGeneric().toString());
+	@Override
+	public void create() {
+		Generic newInstance = ((Type) generic).setInstance(newValue);
+		for (RowComponent row : this.<RowComponent> getChildren()) {
+			List<AbstractComponent> rows = row.getChildren();
+			for (int i = 0; i < rows.size(); i++) {
+				Attribute attribute = (Attribute) ((OutputTextComponent) rows.get(i)).getGeneric();
+				AbstractComponent abstractComponent = rows.get(++i);
+				if (abstractComponent instanceof InputTextComponent) {
+					String value = ((InputTextComponent) abstractComponent).getValue();
+					newInstance.setValue(attribute, value);
+				} else if (abstractComponent instanceof SelectItemComponent) {
+					String value = ((SelectItemComponent) abstractComponent).getValue();
+					Generic instance = ((Type) getGeneric().getOtherTargets((Attribute) ((SelectItemComponent) abstractComponent).getGeneric()).get(0)).getInstance(value);
+					newInstance.bind((Relation) attribute, instance);
+				}
+			}
+		}
+		setNewValue(newValue);
 	}
 
-	public void setInstanceName(String name) {
-		newValue = name;
-	}
-
+	@Override
 	public void modify() {
 		if (!getInstanceName().equals(newValue))
 			setGeneric(getGeneric().setValue(newValue));
@@ -71,15 +87,26 @@ public class EditComponent extends AbstractGenericCollectableChildrenComponent {
 				}
 			}
 		}
+		setNewValue(Objects.toString(getGeneric().toString()));
+	}
+
+	public void exec() {
+		if (getMode().equals(MODE.CREATION))
+			create();
+		else
+			modify();
 	}
 
 	public boolean isRelation() {
 		return getGeneric().isRelation();
 	}
 
-	public String updateMsg() {
-		return "update";
-	}
+	// DATE
+	// public boolean validateDate(String date) {
+	// Pattern p = Pattern.compile(DATE_PATTERN);
+	// Matcher m = p.matcher(date);
+	// return (m.matches());
+	// }
 
 	public String getColumnTitleAttribute() {
 		if (!isRelation())
@@ -88,8 +115,16 @@ public class EditComponent extends AbstractGenericCollectableChildrenComponent {
 			return Objects.toString(((GenericComponent) this.getParent()).getGeneric().<Type> getOtherTargets((Attribute) getGeneric()).get(0).<Class<?>> getValue().getSimpleName());
 	}
 
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
 	@Override
 	public String getXhtmlPath() {
-		return "edit.xhtml";
+		return "/pages/createandedit.xhtml";
 	}
 }
