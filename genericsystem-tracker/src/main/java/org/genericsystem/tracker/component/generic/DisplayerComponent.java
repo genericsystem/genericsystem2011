3983becbe0bcc6ebe25cdeb2d069576ffe01b0fd
Ahmed Jerbi <ajerbi@middlewarefactory.com>
2014-04-15 18:04:35 +0200

    refractor class displayer

--- genericsystem-tracker/src/main/java/org/genericsystem/tracker/component/generic/DisplayerComponent.java
+++ genericsystem-tracker/src/main/java/org/genericsystem/tracker/component/generic/DisplayerComponent.java
@@ -1,28 +1,19 @@
 package org.genericsystem.tracker.component.generic;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 import org.genericsystem.core.Generic;
 import org.genericsystem.framework.component.AbstractComponent;
+import org.genericsystem.framework.component.AbstractDisplayerComponent;
 import org.genericsystem.tracker.component.SelectorCreateEditComponent;
 
-public class DisplayerComponent extends AbstractComponent {
-
-	AbstractComponent child;
+public class DisplayerComponent extends AbstractDisplayerComponent {
 
 	public DisplayerComponent(AbstractComponent parent) {
 		super(parent);
 	}
 
 	@Override
-	public List<? extends AbstractComponent> initChildren() {
-		return child == null ? Collections.emptyList() : Arrays.asList(child);
-	}
-
-	public void buildChild(Generic selected) {
-		child = new SelectorCreateEditComponent(this, selected);
+	public AbstractComponent displayChild(Generic selected) {
 		children = null;
+		return new SelectorCreateEditComponent(this, selected);
 	}
 }
--- genericsystem-tracker/src/main/java/org/genericsystem/tracker/component/generic/DisplayerCreateEditComponent.java
+++ genericsystem-tracker/src/main/java/org/genericsystem/tracker/component/generic/DisplayerCreateEditComponent.java
@@ -1,28 +1,18 @@
 package org.genericsystem.tracker.component.generic;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 import org.genericsystem.core.Generic;
 import org.genericsystem.framework.component.AbstractComponent;
+import org.genericsystem.framework.component.AbstractDisplayerComponent;
 
-public class DisplayerCreateEditComponent extends AbstractComponent {
-
-	AbstractComponent child;
+public class DisplayerCreateEditComponent extends AbstractDisplayerComponent {
 
 	public DisplayerCreateEditComponent(AbstractComponent parent) {
 		super(parent);
 	}
 
 	@Override
-	public List<? extends AbstractComponent> initChildren() {
-		return child == null ? Collections.emptyList() : Arrays.asList(child);
-	}
-
-	public AbstractComponent buildChild(Generic selected) {
-		child = new CreateAndEditComponent(this, selected);
+	public AbstractComponent displayChild(Generic selected) {
 		children = null;
-		return child;
+		return new CreateAndEditComponent(this, selected);
 	}
 }
