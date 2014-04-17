709ddf69f00aae4f13c73a767916478b587245db
Ahmed Jerbi <ajerbi@middlewarefactory.com>
2014-04-17 16:19:28 +0200

    remove static instances

--- genericsystem-tracker/src/main/java/org/genericsystem/tracker/structure/Instances.java
+++ genericsystem-tracker/src/main/java/org/genericsystem/tracker/structure/Instances.java
@@ -1,25 +1,20 @@
 package org.genericsystem.tracker.structure;
 
-import org.genericsystem.annotation.Meta;
-import org.genericsystem.annotation.SystemGeneric;
-import org.genericsystem.annotation.value.StringValue;
-import org.genericsystem.core.GenericImpl;
-import org.genericsystem.tracker.structure.Types.Priorities;
 
 public class Instances {
 
-	@SystemGeneric
-	@Meta(Priorities.class)
-	@StringValue("Minor")
-	public static class Minor extends GenericImpl {
-
-	}
-
-	@SystemGeneric
-	@Meta(Priorities.class)
-	@StringValue("Major")
-	public static class Major extends GenericImpl {
-
-	}
+	// @SystemGeneric
+	// @Meta(Priorities.class)
+	// @StringValue("Minor")
+	// public static class Minor extends GenericImpl {
+	//
+	// }
+	//
+	// @SystemGeneric
+	// @Meta(Priorities.class)
+	// @StringValue("Major")
+	// public static class Major extends GenericImpl {
+	//
+	// }
 
 }
