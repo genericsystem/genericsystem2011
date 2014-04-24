50e6e5e186ae04a597971deedc7e713e8509c48f
mory <mory@middlewarefactory.com>
2014-04-24 12:25:12 +0200

    move vertex to GS V3

--- genericsystem-impl/src/test/java/org/genericsystem/impl/vertex/Statics.java
+++
@@ -1,8 +0,0 @@
-package org.genericsystem.impl.vertex;
-
-public class Statics {
-
-	public static final int NO_POSITION = -1;
-	public final static String ENGINE_VALUE = "Engine";
-	public static final Vertex[] EMPTY_VERTICES = new Vertex[] {};
-}
